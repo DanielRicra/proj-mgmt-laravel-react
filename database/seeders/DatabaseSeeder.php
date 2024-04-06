faab41b9eac3fbe6d7abc4feca5e3d171fa946dc
DanielRicra <daniel.int@outlook.com>
2024-04-05 20:14:24 -0500

    Update DatabaseSeeder to add 2 users, projects and tasks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,29 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+class DatabaseSeeder extends Seeder {
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void {
+    User::factory()->create([
+      'name' => 'Test',
+      'email' => 'test@example.com',
+      'password' => bcrypt('test1234'),
+      'email_verified_at' => time(),
+    ]);
+    User::factory()->create([
+      'name' => 'Daniel',
+      'email' => 'daniel@example.com',
+      'password' => bcrypt('daniel123'),
+      'email_verified_at' => time(),
+    ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    Project::factory()->count(21)->hasTasks(12)->create();
+  }
 }
