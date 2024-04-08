142db3d003636cbf3986677e180af1c083badd0d
DanielRicra <daniel.int@outlook.com>
2024-04-07 21:50:39 -0500

    Add pages/constants file to save constants data

---
+++ resources/js/Pages/constants.ts
@@ -0,0 +1,17 @@
+export const projectStatusClass = {
+  pending: "bg-amber-500",
+  in_progress: "bg-blue-500",
+  completed: "bg-green-500",
+}
+
+export const taskStatusClass = {
+  pending: "bg-amber-500",
+  in_progress: "bg-blue-500",
+  completed: "bg-green-500",
+}
+
+export const taskPriorityClass = {
+  pending: "bg-gray-500",
+  in_progress: "bg-amber-500",
+  completed: "bg-red-500",
+}
