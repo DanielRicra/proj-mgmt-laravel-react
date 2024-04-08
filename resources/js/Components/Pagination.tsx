26244469985189a49a35dfeb689e1aac0de2d24c
DanielRicra <daniel.int@outlook.com>
2024-04-07 21:49:25 -0500

    Add Pagination component for react frontend part

---
+++ resources/js/Components/Pagination.tsx
@@ -0,0 +1,28 @@
+import type { ResponseLink } from "@/types/project";
+import { Link } from "@inertiajs/react";
+
+type PaginationProps = {
+	links: ResponseLink[];
+};
+
+export default function Pagination({ links }: PaginationProps) {
+	return (
+		<nav className="text-center mt-4">
+			{links.map((link) => (
+				<Link
+					key={link.label}
+					dangerouslySetInnerHTML={{ __html: link.label }}
+					href={link.url ?? ""}
+					preserveScroll
+					className={
+						"inline-block py-2 px-3 rounded-lg text-gray-200 text-xs " +
+						(link.active ? "bg-gray-950 " : " ") +
+						(!link.url
+							? "text-gray-500 cursor-not-allowed "
+							: " hover:bg-gray-950")
+					}
+				/>
+			))}
+		</nav>
+	);
+}
