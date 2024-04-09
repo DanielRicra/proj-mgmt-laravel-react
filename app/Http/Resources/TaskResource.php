9b691f2586e9f71b295436e345df74bea82307de
DanielRicra <daniel.int@outlook.com>
2024-04-09 15:03:10 -0500

    Add TaskResource

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource {
+  /**
+   * Transform the resource into an array.
+   *
+   * @return array<string, mixed>
+   */
+  public function toArray(Request $request): array {
+    return [
+      'id' => $this->id,
+      'name' => $this->name,
+      'description' => $this->description,
+      'image_path' => $this->image_path,
+      'status' => $this->status,
+      'priority' => $this->priority,
+      'due_date' => (new Carbon($this->due_date))->toFormattedDateString(),
+      'created_at' => (new Carbon($this->created_at))->toFormattedDateString(),
+      'assigned_user' => new UserResource($this->assignedUserId),
+      'created_by' => new UserResource($this->createdBy),
+      'updated_by' => new UserResource($this->updatedBy),
+      'project_id' => $this->project_id,
+    ];
+  }
+}
