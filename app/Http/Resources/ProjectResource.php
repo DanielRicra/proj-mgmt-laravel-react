b5a460f4ae24ec1f339ef818e9f815c80ecd45f8
DanielRicra <daniel.int@outlook.com>
2024-04-10 10:08:46 -0500

    Refactor  image_path o be Storage::url or ''

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class ProjectResource extends JsonResource {
   public static $wrap = false;
@@ -21,7 +22,7 @@ class ProjectResource extends JsonResource {
       'created_at' => (new Carbon($this->created_at))->toFormattedDateString(),
       'due_date' => (new Carbon($this->due_date))->toFormattedDateString(),
       'status' => $this->status,
-      'image_path' => $this->image_path,
+      'image_path' => $this->image_path ? Storage::url($this->image_path) : '',
       'created_by' => new UserResource($this->createdBy), // To return user object instead of its id in project table
       'updated_by' => new UserResource($this->updatedBy), // To return user object instead of its id in project table
     ];
