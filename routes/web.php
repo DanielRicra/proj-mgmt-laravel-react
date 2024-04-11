14b2846543c03d6f8dbfe8655381153834090054
DanielRicra <daniel.int@outlook.com>
2024-04-11 17:28:01 -0500

    Add new route in wep.php for '{userId}/task'

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
   Route::resource('user', UserController::class);
   Route::resource('project', ProjectController::class);
   Route::resource('task', TaskController::class);
+  Route::get('{userId}/task', [TaskController::class, 'myTasks'])->name('user.myTasks');
 });
 
 Route::middleware('auth')->group(function () {
