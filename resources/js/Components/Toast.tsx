d16d52e9f15489201c9963207dbdf414bb00e0bb
DanielRicra <daniel.int@outlook.com>
2024-04-10 23:10:03 -0500

    Improve  Toast component
    Add message props
    
    Make rerender this component every time message changes

--- resources/js/Components/Toast.tsx
+++ resources/js/Components/Toast.tsx
@@ -4,24 +4,26 @@ type ToastProps = {
 	children: ReactNode;
 	duration?: number;
 	className?: string;
+	message: string;
 };
 
-function Toast({ children, duration = 1000, className }: ToastProps) {
+function Toast({ children, duration = 1000, className, message }: ToastProps) {
 	const [show, setShow] = useState(true);
 
 	useEffect(() => {
+		setShow(true);
 		const timerId = setTimeout(() => {
 			setShow(false);
 		}, duration);
 
 		return () => clearTimeout(timerId);
-	}, [duration]);
+	}, [duration, message]);
 
-	return (
+	return show ? (
 		<div className={className} style={{ display: show ? "block" : "none" }}>
 			{children}
 		</div>
-	);
+	) : null;
 }
 
 export default Toast;
